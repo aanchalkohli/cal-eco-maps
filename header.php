b13b606551785c03acd6a11538e3634a4102a5fa
scottgruber <scott@pixelfix.com>
2015-03-12 13:13:29 -0700

    Changed the file capitalization and names to all lower case.
    Signed-off-by: scottgruber <scott@pixelfix.com>

---
+++ header.php
@@ -0,0 +1,114 @@
+<div id="headerlogos" style="width:350px",><a href="http://www.environment.ucla.edu/"><img src="assets/img/ucla-ioes-logo.png"  alt="IOES" width=300px/></a>
+<a href="http://www.environment.ucla.edu/ccep/"><img src="assets/img/cceplogo.jpg"   alt="IOES" width=300px/></a></div>
+<a href="index.php"><img src="assets/img/logo.png" alt="CalEco Maps" border="0" /></a>
+
+<nav>
+	<!--<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="aboutus.php">About Us</a></li>
+		<li><a href="#">Methodology</a></li>
+		<li><a href="#">Rankings</a></li>
+		<li><a href="#">Data</a></li>
+		<li><a href="#">Feedback</a></li>
+	</ul>-->
+	<script src="assets/js/bootstrap.min.js"></script>
+	
+<link href="assets/css/vendor/bootstrap/bootstrap.css" rel="stylesheet">
+<link rel="stylesheet" href="assets/css/style.css" />	
+ <!-- Static navbar -->
+    <div class="bootstrap"> 
+      <div class="navbar navbar-custom" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Cal EcoMaps</a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+			  <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Methodology <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+				<li class="divider"></li>
+				  <li><t><a href="eismethodology.php" style="color: DarkGreen">Cal EcoMaps Environmental Impact Score</a></t></li>
+				  <li class="divider"></li>
+                  <li><a href="methodology.php">Scope</a></li>
+				  <li class="divider"></li>
+				  <li><t><a href="eri.php" style="color: DarkGreen">Environmental Impact Indicators</a></t></li>
+				  <li class="divider"></li>
+                  <li><a href="totaltoxicreleases.php">Total Toxic Releases</a></li>
+                  <li><a href="rpkr.php">Releases per $1000 Revenue</a></li>
+				  <li><a href="toxicity.php">Toxicity of Total Releases</a></li>
+				  <li><a href="cancermethod.php">Regional Contribution to Lifetime Cancer Risk </a></li>
+                  <!--<li class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>-->
+                  <li><a href="ppwma.php">Preferred Waste Management Activities</a></li>
+                  
+				  
+				  
+                </ul>
+              </li>
+           
+			
+			  <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Impact Score <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+				<li class="divider"></li>
+				<li><t>Environmental Impact Score Ranking Lists</t></li>
+				  <li class="divider"></li>
+                  <li><a href="petroscores.php">Petroleum </a></li>
+                  <li><a href="fmscores.php">Fabricated Metals </a></li>
+                  <li><a href="pmscores.php">Primary Metals </a></li>
+                  <!--<li class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>-->
+				  
+                  <li><a href="chemscores.php">Chemicals </a></li>
+				  <li class="divider"></li>
+				  <li class="divider"></li>
+				  <li><t>Industry Top 10 Facilities Graphs</t></li>
+				  <li class="divider"></li>
+				  <li><a href="pmranking.php">Primary Metals</a></li>
+                  <li><a href="petroranking.php">Petroleum</a></li>
+                  <li><a href="fmranking.php">Fabricated Metals</a></li>
+                  <!--<li class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>-->
+                  <li><a href="chemicalsranking.php">Chemicals</a></li>
+				  
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Downloads<b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="report.php">Report</a></li>
+				  <li><a href="factsheet.php">Fact Sheet</a></li>
+				  <li><a href="powerpoint.php">Powerpoint Summary</a></li>
+				  <li><a href="manual.php">Site Manual</a></li>
+				  <li><a href="mastersheet.php">Data Sheets</a></li>
+                  <!--<li><a href="">Excel Data</a></li>-->
+               
+				  
+                </ul>
+              </li>
+			  
+			  <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us<b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="aboutus.php">Project Members</a></li>
+				  <li><a href="acknowledgements.php">Acknowledgements</a></li>
+                  <!--<li><a href="">Excel Data</a></li>-->
+               
+				  
+                </ul>
+              </li>
+			  <li><a href="feedback.php">Feedback</a></li>
+              
+            </ul>
+                     </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </div>
+	  </div>
+	  
